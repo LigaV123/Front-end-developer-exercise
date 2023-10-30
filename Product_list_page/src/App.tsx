6f734a3447c68541329772ad578fdb8113aa2100
Līga Viļumsone <piga456456@gmail.com>
2023-10-30 13:22:45 +0200

    made debounced search

--- Product_list_page/src/App.tsx
+++ Product_list_page/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import './App.css'
+import debounce from 'lodash.debounce'
 import Product from './components/Product'
-//import SearchedProductList from './components/SearchedProductList'
+import ProductDetails from './components/ProductDetails'
+import './App.css'
 
 type Product = {
   id: number,
@@ -20,58 +21,55 @@ function App() {
       axios.get('https://run.mocky.io/v3/b54fe93f-f5a1-426b-a76c-e43d246901fd').then((res) =>{
       setProducts(res.data.products);
       })
-    }, [])
+    }, []);
+
+    const debouncedSearch = debounce((query: string) => {
+      const lowerCaseQuery = query.toLowerCase();
+      const filtered = products.filter(
+        (product) => product.name.toLowerCase().includes(lowerCaseQuery) ||
+        product.category.toLowerCase().includes(lowerCaseQuery)
+      );
+      setSearchedProducts(filtered);
+    }, 1000);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const query = event.target.value;
+      setInputValue(query);
+      debouncedSearch(query);
+    };
 
   return (
     <>
       <div className="search">
-        <form 
-          className="search__form"
-          {(e) => {
-            e.preventDefault();
-            console.log(inputValue);
-            setSearchedProducts(
-              products.filter((val) => val.name.toLowerCase().includes(inputValue.toLowerCase()))
-            );
-            setInputValue('')
-          }}
-        />
         <input 
         type='text' 
-        placeholder='Search products'
+        placeholder='Search by name/category'
         className="search__input"
         value={inputValue}
-        onChange={(eventObj) => {
-          setInputValue(eventObj.target.value)
-        }}
+        onChange={handleInputChange}
         required
         />
-        <div className="search__result">{
-  
-          searchedProducts.map(({id, name, price, category})=> {
-            return (
-              <Product 
-              key={id}
-              name={name}
-              price={price}
-              category={category}
-            />
-            )
-          })
-        }</div>
       </div>
 
-      <div className='products'>
-        {products.map(({id, name, price, category}) => {
-          return (
-            <Product 
-              key={id}
-              name={name}
-              price={price}
-              category={category}
-            />
-          )
-        })}
+      <div className="products">
+        {searchedProducts.length > 0 
+          ? searchedProducts.map(({ id, name, price, category }) => (
+              <Product
+                key={id}
+                name={name}
+                price={price}
+                category={category}
+              />
+            ))
+          : products.map(({ id, name, price, category }) => (
+              <Product
+                key={id}
+                name={name}
+                price={price}
+                category={category}
+              />
+            ))
+        }
       </div>
     </>
   )
